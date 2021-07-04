8d918aee6e422834e6aea3c6f477f5f4f4919605
david-sanvicente <sanvicented1@gmail.com>
2021-07-04 15:34:05 -0400

    factored useState and useEffect hooks into HomeScreen and ProductScreen

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,9 +1,19 @@
-import React from 'react'
-import products from '../products'
+import React, { useState, useEffect } from 'react'
 import Product from '../components/Product'
 import { Row, Col } from 'react-bootstrap'
+import axios from 'axios'
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products')
+      setProducts(data)
+    }
+    fetchProducts()
+  }, [])
+
   return (
     <div>
       <h1>Latest Products</h1>
@@ -11,7 +21,6 @@ const HomeScreen = () => {
         {products.map((product) => {
           return (
             <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              {/* <h3>{prod.name}</h3> */}
               <Product product={product} />
             </Col>
           )
