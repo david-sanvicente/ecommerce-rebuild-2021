1cc8028a137673d8135684c0826a552419c065f1
david-sanvicente <sanvicented1@gmail.com>
2021-07-11 13:40:04 -0400

    custom authentication middleware

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -21,4 +21,21 @@ const authUser = asyncHandler(async (req, res) => {
   }
 })
 
-export default authUser
+const getUserProfile = asyncHandler(async (req, res) => {
+  // res.send('Success!')
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+export { authUser, getUserProfile }
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import asyncHandler from 'express-async-handler'
 import Users from '../models/Users.js'
-import authUser from '../controllers/userController.js'
+import { authUser, getUserProfile } from '../controllers/userController.js'
+import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.post('/login', authUser)
+router.route('/profile').get(protect, getUserProfile)
 
 export default router
