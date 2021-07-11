eb5b8e9c29a4e97603a1e53f5bc1975392c87dee
david-sanvicente <sanvicented1@gmail.com>
2021-07-11 14:04:50 -0400

    user registration and password encryption

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,16 @@
 import express from 'express'
 import asyncHandler from 'express-async-handler'
 import Users from '../models/Users.js'
-import { authUser, getUserProfile } from '../controllers/userController.js'
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
+router.route('/').post(registerUser)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile)
 
