2353cd8e2d25813df2b09cf72ec26fc1ffcd502a
david-sanvicente <sanvicented1@gmail.com>
2021-08-02 20:23:58 -0400

    product pagination and admin product pagination

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -5,18 +5,21 @@ import Product from '../components/Product'
 import { Row, Col } from 'react-bootstrap'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
+import Paginate from '../components/Paginate'
 
 const HomeScreen = ({ match }) => {
   const keyword = match.params.keyword
 
+  const pageNumber = match.params.pageNumber || 1
+
   const dispatch = useDispatch()
 
   const productList = useSelector((state) => state.productList)
-  const { loading, error, products } = productList
+  const { loading, error, products, page, pages } = productList
 
   useEffect(() => {
-    dispatch(listProducts(keyword))
-  }, [dispatch, keyword])
+    dispatch(listProducts(keyword, pageNumber))
+  }, [dispatch, keyword, pageNumber])
 
   return (
     <div>
@@ -26,22 +29,34 @@ const HomeScreen = ({ match }) => {
       ) : error ? (
         <Message variant='danger'>{error}</Message>
       ) : (
-        <Row>
-          {products.map((product) => {
-            return (
-              <Col
-                key={product._id}
-                sm={12}
-                md={6}
-                lg={4}
-                xl={3}
-                className='align-items-stretch d-flex'
-              >
-                <Product product={product} />
-              </Col>
-            )
-          })}
-        </Row>
+        <>
+          <Paginate
+            pages={pages}
+            page={page}
+            keyword={keyword ? keyword : ''}
+          />
+          <Row>
+            {products.map((product) => {
+              return (
+                <Col
+                  key={product._id}
+                  sm={12}
+                  md={6}
+                  lg={4}
+                  xl={3}
+                  className='align-items-stretch d-flex'
+                >
+                  <Product product={product} />
+                </Col>
+              )
+            })}
+          </Row>
+          <Paginate
+            pages={pages}
+            page={page}
+            keyword={keyword ? keyword : ''}
+          />
+        </>
       )}
     </div>
   )
--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -2,6 +2,9 @@ import asyncHandler from 'express-async-handler'
 import Product from '../models/Products.js'
 
 const getProducts = asyncHandler(async (req, res) => {
+  const pageSize = 12
+  const page = Number(req.query.pageNumber) || 1
+
   const keyword = req.query.keyword
     ? {
         name: {
@@ -11,8 +14,13 @@ const getProducts = asyncHandler(async (req, res) => {
       }
     : {}
 
+  const count = await Product.countDocuments({ ...keyword })
+
   const products = await Product.find({ ...keyword })
-  res.json(products)
+    .limit(pageSize)
+    .skip(pageSize * (page - 1))
+
+  res.json({ products, page, pages: Math.ceil(count / pageSize) })
 })
 
 const getProductById = asyncHandler(async (req, res) => {
